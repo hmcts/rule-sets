58583bace4ad4c5ff806124df434336d7d0823a9
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-07-01 14:59:16 +0100

    Script and workflow added

---
+++ scripts/update-repo-list.py
@@ -0,0 +1,54 @@
+import requests
+import yaml
+import json
+import re
+import os
+
+# URLs to the remote YAML and JSON files
+urls = [
+    'https://raw.githubusercontent.com/hmcts/sds-jenkins-config/master/environment-approvals.yml',
+    'https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/environment-approvals.yml',
+    'https://raw.githubusercontent.com/hmcts/cnp-jenkins-config/master/terraform-infra-approvals/global.json',
+    'https://raw.githubusercontent.com/hmcts/sds-jenkins-config/master/terraform-infra-approvals/global.json',
+]
+
+# Function to fetch and parse the files
+def fetch_and_parse(url):
+    response = requests.get(url)
+    if url.endswith('.yml') or url.endswith('.yaml'):
+        return yaml.safe_load(response.text)
+    elif url.endswith('.json'):
+        return json.loads(response.text)
+    return None
+
+# Collect all repositories from the parsed files
+all_repos = []
+
+for url in urls:
+    data = fetch_and_parse(url)
+    if url.endswith('.yml') or url.endswith('.yaml'):
+        # For YAML files
+        for key in data:
+            if isinstance(data[key], list):
+                for item in data[key]:
+                    if 'repo' in item:
+                        all_repos.append(item['repo'])
+    elif url.endswith('.json'):
+        # For JSON files
+        if 'module_calls' in data:
+            for item in data['module_calls']:
+                if 'source' in item:
+                    # Extract the repository URL from the 'source' value
+                    match = re.search(r'git@github.com:(.*?)(\.git|$)', item['source'])
+                    if match:
+                        all_repos.append(f'https://github.com/{match.group(1)}')
+
+# Remove duplicates
+all_repos = list(set(all_repos))
+
+# Determine the path for the output file
+repo_file = os.path.join(os.path.dirname(__file__), '../production-repos.json')
+
+# Update the local file
+with open(repo_file, 'w') as f:
+    json.dump(all_repos, f, indent=2)
