9d3368759ff2072c796fd79ec14da46385f9228d
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-03 23:07:04 +0100

    adding config + pipeline

--- components/main.tf
+++ components/main.tf
@@ -1,27 +1,3 @@
-locals {
-  # Read the repositories list from the JSON file
-  repositories_list = jsondecode(file("${path.module}/test-repos.json"))
-
-  # Create a combination of repositories and branches
-  repo_branch_combinations = flatten([
-    for repo in local.repositories_list : [
-      for branch in var.branches : {
-        repo   = repo
-        branch = branch
-      }
-    ]
-  ])
-}
-
-variable "branches" {
-  description = "List of branches to apply protection rules"
-  type        = list(string)
-  default     = [
-    "master",
-    "main"
-  ]
-}
-
 # Check if branches exist
 data "github_branch" "existing_branches" {
   for_each = { for combo in local.repo_branch_combinations : "${combo.repo}:${combo.branch}" => combo }
