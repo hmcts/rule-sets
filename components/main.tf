e943b43dcba87edbe24e454560caf7753e0b6c00
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-03 23:09:42 +0100

    adding config + pipeline

--- components/main.tf
+++ components/main.tf
@@ -5,6 +5,28 @@ module "tags" {
   builtFrom   = var.builtFrom
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = module.tags.common_tags
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = module.tags.common_tags
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+
+}
+
 # Check if branches exist
 data "github_branch" "existing_branches" {
   for_each = { for combo in local.repo_branch_combinations : "${combo.repo}:${combo.branch}" => combo }
