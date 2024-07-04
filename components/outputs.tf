1cfc096d90b1092a8e171c81ba73d4dda6a32c60
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-04 10:45:28 +0100

    adding config + pipeline

--- components/outputs.tf
+++ components/outputs.tf
@@ -13,3 +13,10 @@ output "included_repositories" {
 output "repo_branch_combinations" {
   value = local.repo_branch_combinations
 }
+
+output "valid_branch_combinations" {
+  value = {
+    for combo in local.repo_branch_combinations : "${combo.repo}:${combo.branch}" => combo
+    if try(data.github_branch.existing_branches["${combo.repo}:${combo.branch}"].branch, null) != null
+  }
+}
