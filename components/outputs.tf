8a5de5002f6d544820faeccf58cac0791e5c5b77
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-05 11:45:16 +0100

    adding config + pipeline

--- components/outputs.tf
+++ components/outputs.tf
@@ -6,18 +6,18 @@ output "common_tags" {
   }
 }
 
-output "included_repositories" {
-  value = local.included_repositories
-}
+# output "included_repositories" {
+#   value = local.included_repositories
+# }
 
-output "repo_branch_combinations" {
-  value = local.repo_branch_combinations
-}
+# output "repo_branch_combinations" {
+#   value = local.repo_branch_combinations
+# }
 
-output "valid_branch_combinations" {
-  value = {
-    for combo in local.repo_branch_combinations : "${combo.repo}:${combo.branch}" => combo
-    if try(data.github_branch.existing_branches["${combo.repo}:${combo.branch}"].branch, null) != null
-  }
-}
+# output "valid_branch_combinations" {
+#   value = {
+#     for combo in local.repo_branch_combinations : "${combo.repo}:${combo.branch}" => combo
+#     if try(data.github_branch.existing_branches["${combo.repo}:${combo.branch}"].branch, null) != null
+#   }
+# }
 
