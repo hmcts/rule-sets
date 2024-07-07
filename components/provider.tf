7ad12f1bb228dac9cecf2fa2ba82f48ab2b9bd38
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-07 22:39:28 +0100

    adding config + pipeline

--- components/provider.tf
+++ components/provider.tf
@@ -15,8 +15,8 @@ terraform {
     storage_account_name = "rulesetsa"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc = true
-    use_azuread_auth = true
+    use_oidc             = true
+    use_azuread_auth     = true
   }
 
   required_providers {
