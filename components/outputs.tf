aaba4b1bf6485918f08a81a31d6cfe0a5b3640cd
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-09 17:21:23 +0100

    testing prod repos plan

--- components/outputs.tf
+++ components/outputs.tf
@@ -6,3 +6,18 @@ output "common_tags" {
   }
 }
 
+output "raw_repositories_list" {
+  value = local.raw_repositories_list
+}
+
+output "repositories_list" {
+  value = local.repositories_list
+}
+
+output "included_repositories" {
+  value = local.included_repositories
+}
+
+output "repo_branch_combinations" {
+  value = local.repo_branch_combinations
+}
