0a307219520cb9128341910f63fed5b7cc72cff1
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-17 00:22:34 +0100

    updating file path

--- components/data.tf
+++ components/data.tf
@@ -5,3 +5,12 @@ data "github_organization" "org" {
 data "local_file" "repos_json" {
   filename = "${path.module}./production-repos.json"
 }
+
+data "github_branch" "existing_branches" {
+  for_each = {
+    for combo in local.repo_branch_combinations : "${combo.repo}:${combo.branch}" => combo
+  }
+  
+  repository = each.value.repo
+  branch     = each.value.branch
+}
