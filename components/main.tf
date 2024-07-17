275bb28875905baa61022b951ee89b6f0b21eb21
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-17 15:59:21 +0100

    testing file

--- components/main.tf
+++ components/main.tf
@@ -28,9 +28,7 @@ resource "azurerm_storage_container" "tfstate" {
 
 # Create rulesets for repositories with existing branches
 resource "github_repository_ruleset" "default_ruleset" {
-  for_each = {
-    for repo in local.included_repositories : repo => repo
-  }
+  for_each = toset(local.included_repositories)
 
   repository  = each.key
   name        = "Branch Protection Rule Sets"
