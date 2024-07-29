3dd9442d03fe8df57b6bbe10a23e9e6cbab83cf4
Connor O'Kane <connor.o'kane@justice.gov.uk>
2024-07-29 15:12:37 +0100

    adding python code

--- set_branch_protection.py
+++ set_branch_protection.py
@@ -19,7 +19,10 @@ headers = {
 def load_repositories(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
-    return data['repositories']
+        if isinstance(data, list):
+            return data
+        else:
+            raise ValueError("Unexpected JSON format. Expected a list of repositories.")
 
 # Function to get the current ruleset
 def get_ruleset(org, ruleset_id):
@@ -35,17 +38,25 @@ def update_ruleset(org, ruleset_id, data):
     response.raise_for_status()
     return response.json()
 
-# Load repositories from JSON file
-target_repositories = load_repositories(REPO_FILE)
+try:
+    # Load repositories from JSON file
+    target_repositories = load_repositories(REPO_FILE)
+    print(f"Loaded repositories: {target_repositories}")
 
-# Get the current ruleset data
-ruleset = get_ruleset(ORGANIZATION, RULESET_ID)
+    # Get the current ruleset data
+    ruleset = get_ruleset(ORGANIZATION, RULESET_ID)
+    print("Current Ruleset:")
+    print(json.dumps(ruleset, indent=4))
 
-# Update the repository_name condition
-ruleset['conditions']['repository_name']['include'] = target_repositories
+    # Update the repository_name condition
+    ruleset['conditions']['repository_name']['include'] = target_repositories
 
-# Update the ruleset on GitHub
-updated_ruleset = update_ruleset(ORGANIZATION, RULESET_ID, ruleset)
+    # Update the ruleset on GitHub
+    updated_ruleset = update_ruleset(ORGANIZATION, RULESET_ID, ruleset)
 
-print("Updated Ruleset:")
-print(json.dumps(updated_ruleset, indent=4))
+    print("Updated Ruleset:")
+    print(json.dumps(updated_ruleset, indent=4))
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+    sys.exit(1)
