c2ed0d6e036bf5ca37c07e55b2d49c38a1f0cec3
ciaranca <c.carragher@kainos.com>
2024-07-30 15:46:17 +0100

    change token ref (#33)

--- scripts/update-ruleset-tagets.py
+++ scripts/update-ruleset-tagets.py
@@ -3,14 +3,14 @@ import json
 import os
 
 # Configuration
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
 ORGANIZATION = 'hmcts-test'
 RULESET_ID = '1239224'
 REPO_FILE = 'scripts/production_repos.json'
 
 # Headers for authentication
 headers = {
-    'Authorization': f'Bearer {GITHUB_TOKEN}',
+    'Authorization': f'Bearer {OAUTH_TOKEN}',
     'Accept': 'application/vnd.github+json',
     'Content-Type': 'application/json'
 }
