ff7d44cca4c670287dd03c664a03013c8061ae11
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-07-30 15:54:46 +0100

    change token ref

--- scripts/update-ruleset-targets.py
+++ scripts/update-ruleset-targets.py
@@ -3,14 +3,14 @@ import json
 import os
 
 # Configuration
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
 ORGANIZATION = 'hmcts-test'
 RULESET_NAME = 'test-ruleset'
 REPO_FILE = 'production-repos.json'
 
 # Headers for authentication
 headers = {
-    'Authorization': f'Bearer {GITHUB_TOKEN}',
+    'Authorization': f'Bearer {OAUTH_TOKEN}',
     'Accept': 'application/vnd.github+json',
     'Content-Type': 'application/json',
     'X-GitHub-Api-Version': '2022-11-28'
